270b548c8127f0fd0f0292358a40a29ccd18849f
Anthony Mills <anthonymills4421@gmail.com>
2025-09-27 21:52:09 -0400

    Fixed an import issue in urls.py

--- MentalHealthApp/backend/backend/tasks/urls.py
+++ MentalHealthApp/backend/backend/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CompareClean
+from .views import compare_clean
 
 urlpatterns = [
     path('compare-clean/', compare_clean.as_view(), name='compare-clean'),
